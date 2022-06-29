8ebbb8b035bfe0d26e5c9147e8252425a04c9100
Agustin Luis <AOLuis@student.fullsail.edu>
2022-06-29 18:01:49 -0400

    I finish

--- Game.cpp
+++ Game.cpp
@@ -20,13 +20,17 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+	for (int i = 0; i < 5; i++)
+	{
+		Box brick;
+		brick.width = 10;
+		brick.height = 2;
+		brick.x_position = i * (Console::WindowWidth() / 5);
+		brick.y_position = 5;
+		brick.doubleThick = true;
+		brick.color = ConsoleColor::DarkGreen;
+		bricks.push_back(brick);
+	}
 }
 
 void Game::ResetBall()
@@ -70,25 +74,48 @@ void Game::Render() const
 	ball.Draw();
 
 	// TODO #3 - Update render to render all bricks
-	brick.Draw();
-
+	for (int i = 0; i < bricks.size(); i++)
+	{
+		bricks[i].Draw();
+	}
 	Console::Lock(false);
+	if (ball.y_position == Console::WindowHeight())
+	{
+		Console::SetCursorPosition(20, 20);
+		std::cout << "You Suck, Imagine Losing... Press 'R' to try again!";
+	}
+	if (bricks.size() == 0)
+	{
+		Console::SetCursorPosition(20, 20);
+		std::cout << "What A Chad, You Are Now The Victor! Press 'R' to try again!";
+	}
 }
 
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+	for (int i = 0; i < bricks.size(); i++)
 	{
-		brick.color = ConsoleColor(brick.color - 1);
-		ball.y_velocity *= -1;
+		if (bricks[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+		{
+			bricks[i].color = ConsoleColor(bricks[i].color - 1);
+			ball.y_velocity *= -1;
 
-		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-
+			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+			if (bricks[i].color == Black)
+			{
+				bricks.erase(bricks.begin() + i);
+			}
+		}
 	}
 
 	// TODO #6 - If no bricks remain, pause ball and display victory text with R to reset
-
+	if (bricks.size() == 0)
+	{
+		ball.moving = false;
+		Console::SetCursorPosition(20, 20);
+		std::cout << "What A Chad, You Are Now The Victor! Press 'R' to try again!";
+	}
 
 	if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
 	{
@@ -96,4 +123,10 @@ void Game::CheckCollision()
 	}
 
 	// TODO #7 - If ball touches bottom of window, pause ball and display defeat text with R to reset
+	if (ball.y_position == Console::WindowHeight())
+	{
+		ball.moving = false;
+		Console::SetCursorPosition(20, 20);
+		std::cout << "You Suck, Imagine Losing... Press 'R' to try again!";
+	}
 }
